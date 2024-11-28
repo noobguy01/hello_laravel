09759d29038dfce19c23a43b1368a7759008e5cc
noobguy01 <2200261141@qq.com>
2024-11-28 23:20:18 +0800

    Eloquent

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticPagesController;
 Route::get('/',[StaticPagesController::class,'home'])->name('home');
 Route::get('/help', [StaticPagesController::class,'help'])->name('help');
 Route::get('/about', [StaticPagesController::class,'about'])->name('about');
+Route::get('signup', [UserController::class,'create'])->name('signup');
