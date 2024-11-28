250538c48bb30fcd695bc62a2b4b5593df8e744b
noobguy01 <2200261141@qq.com>
2024-11-28 18:22:35 +0800

    sass样式

--- Laravel/resources/views/static_page/home.blade.php
+++ Laravel/resources/views/static_page/home.blade.php
@@ -2,5 +2,14 @@
 @section('title','home')
 
 @section('content')
-<h1>主页</h1>
+<div class="my_bg">
+<h1>Hello Laravel</h1>
+<p class="lead">你现在所看到的是 </p>
+<p>一切，将从这里开始。</p>
+<p>
+
+<a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+</p>
+
+</div>
 @endsection
--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticPagesController;
-Route::get('/', [StaticPagesController::class,'home']);
+Route::get('/',[StaticPagesController::class,'home']);
 Route::get('/help', [StaticPagesController::class,'help']);
 Route::get('/about', [StaticPagesController::class,'about']);
